4f38335f74c553bf30de3ab4628bdfa5730bb670
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-13 00:27:02 -0400

    Nimble bar UI done

--- extension/nimble.js
+++ extension/nimble.js
@@ -1,6 +1,33 @@
 (function () {
+  
+  var nimbleBar = $('<div></div>', {id: 'nimble-bar'});
+  nimbleBar.append($('<input>', {class: 'nimble-input mousetrap'}));
+  $('body').append(nimbleBar);
+  window.nimbleBar = nimbleBar;
+
+  var shown = false;
+
+  function showNimbleBar () {
+    nimbleBar.addClass('visible');
+    $('.nimble-input').focus();
+    $('.nimble-input').val('');
+    shown = true;
+  }
+
+  function hideNimbleBar () {
+    nimbleBar.removeClass('visible');
+    $('.nimble-input').val('');
+    shown = false;
+  }
+
   Mousetrap.bind('shift+n', function(e) {
-    console.log('Nimble triggered');
+    e.preventDefault();
+    shown ? hideNimbleBar() : showNimbleBar();
   });
-  console.log('Nimble finish loading');
+
+  Mousetrap.bind('esc', function(e) {
+    hideNimbleBar();
+  });
+
+  console.log('Nimble loaded');
 })();
