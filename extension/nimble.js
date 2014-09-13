f62d9e0a58afa9292a561121cec56c2e1f5804f4
Camillus Gerard Cai <cgcai@qxcg.net>
2014-09-13 01:01:33 -0400

    Plugin system working example.

--- extension/nimble.js
+++ extension/nimble.js
@@ -3,11 +3,14 @@
     console.log('Nimble triggered');
 
     // This is a data object before it enters the current pipeline stage.
-    var testObj = {};
+    var testObj = {
+      'type': 'text'
+    };
     
     // Matching the object against the recipe manifest yields a list of
     // compatible recipes that may be applied.
     var matchResults = router.matchObject(testObj);
+    console.log(matchResults);
     
     // Via some UI, the user decides on a recipe to apply.
     var selectedMatch = matchResults[0];
@@ -15,27 +18,28 @@
     // We apply the recipe to the data object.
     // In reality, the resultant object should be passed back to the pipeline,
     // until we reach the last stage, at which point, the result is discarded.
-    var execResult = selectedMatch.ref.applyTo(testObj);
+    var execResult = selectedMatch.callback(testObj);
     console.log(execResult);
   });
 
-  var recipes = [
-    {
-      'initCallback': 'TestRecipe()',
-    }
+  var plugins = [
+    'TestPlugin'
   ];
 
   /* Creates instances of recipe workers for each entry in a recipe manifest.
    */
-  var initRecipes = function(recipes) {
-    for (var i = 0; i < recipes.length; i++) {
-      recipes[i].ref = eval(recipes[i].initCallback);
+  var initPlugins = function(plugins) {
+    var recipes = [];
+    for (var i = 0; i < plugins.length; i++) {
+      var construct = plugins[i] + '()';
+      var pluginRef = eval(construct);
+      var supportedRecipes = pluginRef.getRecipes();
+      recipes = recipes.concat(supportedRecipes);
     }
+    return recipes;
   };
 
-  
-
-  initRecipes(recipes);
+  var recipes = initPlugins(plugins);
   var router = Router(recipes);
   console.log('Nimble finish loading');
 })();
--- extension/router.js
+++ extension/router.js
@@ -1,7 +1,19 @@
 var Router = function(recipes) {
 
+  var _matches = function(value) {
+    for (var i = 0; i < value.inputs.length; i++) {
+      var supportedType = value.inputs[i];
+
+      // TODO: RegEx matching.
+      if (this.type == supportedType.type) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   var _matchObject = function(dataObject) {
-    var matchedRecipes = [recipes[0]];
+    var matchedRecipes = recipes.filter(_matches, dataObject);
     return matchedRecipes;
   };
 
