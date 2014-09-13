a6ee972b27eeccc1a22fa16aa1e779b9897ce85d
Camillus Gerard Cai <cgcai@qxcg.net>
2014-09-13 09:56:33 -0400

    Implemented object factories.

--- extension/plugins/googl.js
+++ extension/plugins/googl.js
@@ -17,11 +17,7 @@ var Googl = function() {
         },
         'dataType': 'json',
         'success': function(data) {
-          var retVal = {
-            'type': '"url"',
-            'data-serialization': '"text"',
-            'data': Utils.escapeString(data.id)
-          };
+          var retVal = Factory.newUrl(data.id);
           resolve(retVal);
         },
         'error': function(data, status) {
