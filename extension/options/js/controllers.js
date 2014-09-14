6150764dfaa1b98d25ac13d0c6945d6586a204fc
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-14 04:09:17 -0400

    First pass at accounts page

---
+++ extension/options/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('NimbleApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $('.logo').addClass('animated fadeInLeft');
+    $('.caption').addClass('animated fadeInRight');
+    var t = 1;
+    $('.service-icon').each(function () {
+      var that = this;
+      (function (delay) {
+        setTimeout(function () {
+          $(that).addClass('animated bounceInUp');
+        }, delay * 300);
+      })(t);
+      t += 1;
+    });
+  }])
+  .controller('AccountsCtrl', ['$scope', function ($scope) {
+    
+  }]);
--- extension/options/js/main.js
+++ extension/options/js/main.js
@@ -1,1 +1,16 @@
-
+angular.module('NimbleApp', [  
+  'ngRoute'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/accounts', {
+        templateUrl: 'views/accounts.html',
+        controller: 'AccountsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
