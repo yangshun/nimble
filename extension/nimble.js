5abae4f52ea71cd64f36c3b114267c5e01ef4d2f
Keng Kiat Lim <kkin@crnrc.com>
2014-09-13 20:28:21 -0400

    Clears pipeline when user closes the bar

--- extension/nimble.js
+++ extension/nimble.js
@@ -55,7 +55,7 @@
   }
 
   function hideNimbleBar () {
-    
+    $('.nimble-pipeline').empty();
     nimbleBar.addClass('bounceOutDown');
     setTimeout(function () {
       $('.nimble-input').blur();
