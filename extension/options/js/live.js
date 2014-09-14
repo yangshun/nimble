61b108f1d4c4c21e3498c14efa7727b748b6101e
Camillus Gerard Cai <cgcai@qxcg.net>
2014-09-14 08:04:15 -0400

    .

--- extension/options/js/live.js
+++ extension/options/js/live.js
@@ -1,3 +1,4 @@
 var signInWindowsLive = function() {
+  console.log('in: signInWindowsLive');
   location.href = 'https://login.live.com/oauth20_authorize.srf?client_id=000000004012A7CB&scope=office.onenote_create&response_type=token&redirect_url=http://yangshun.im/nimble/extension/options/redirect.html'
 };
